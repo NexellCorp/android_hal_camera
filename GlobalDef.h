8cae6c6f65457b37a8425d7e14991040eb379769
HyejungKwon <cjscld15@nexell.co.kr>
2018-12-12 17:24:55 +0900

    Add deinterlacing for interlaced camera
    This patch adds deinterlacer to support interlaced camera.
    this feature enabled when following feature is enabled
    in BoardConfigCommon.mk
    :BOARD_CAMERA_BACK_INTERLACED or BOARD_CAMERA_FRONT_INTERLACED
    for deinterlacing,
    MAX_BUFFER_COUNT musb be bigger than 1 and smaller than 4
    
    Change-Id: I5958dd5712534534dd35c73ac1ca94eba412afae
    Signed-off-by: HyejungKwon <cjscld15@nexell.co.kr>

--- GlobalDef.h
+++ GlobalDef.h
@@ -1,7 +1,8 @@
 #ifndef GLOBAL_DEF_H
 #define GLOBAL_DEF_H
 
-#define MAX_BUFFER_COUNT		4
+/* MAX_BUFFER_COUNT musb be bigger than 1 and smaller than 4 for deinterlacing */
+#define MAX_BUFFER_COUNT		3
 #define NX_MAX_STREAM			4
 #define MAX_VIDEO_HANDLES		2
 
@@ -29,6 +30,12 @@
 #define BACK_CAMERA_INTERLACED		""
 #endif
 
+#ifdef BOARD_CAMERA_BACK_COPY_MODE
+#define BACK_CAMERA_COPY_MODE		BOARD_CAMERA_BACK_COPY_MODE
+#else
+#define BACK_CAMERA_COPY_MODE		""
+#endif
+
 #ifdef BOARD_CAMERA_FRONT_DEVICE
 #define FRONT_CAMERA_DEVICE		BOARD_CAMERA_FRONT_DEVICE
 #else
@@ -47,17 +54,23 @@
 #define FRONT_CAMERA_INTERLACED		""
 #endif
 
+#ifdef BOARD_CAMERA_FRONT_COPY_MODE
+#define FRONT_CAMERA_COPY_MODE		BOARD_CAMERA_FRONT_COPY_MODE
+#else
+#define FRONT_CAMERA_COPY_MODE		""
+#endif
+
 #ifdef BOARD_NUM_OF_SKIP_FRAMES
 #define NUM_OF_SKIP_FRAMES		BOARD_NUM_OF_SKIP_FRAMES
 #else
 #define NUM_OF_SKIP_FRAMES		0
 #endif
 
-/*#define TRACE_STREAM*/
+/* #define TRACE_STREAM */
 #ifdef TRACE_STREAM
 #define ALOGDD(a...)			ALOGD(a)
 #define ALOGDI(a...)			ALOGD(a)
-#define ALOGDV(a...)			/*ALOGD(a)*/
+#define ALOGDV(a...)			/* ALOGD(a) */
 #else
 #define ALOGDD(a...)
 #define ALOGDI(a...)
